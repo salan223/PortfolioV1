4a8e0a494ed79202feb55e84ca4a48ab7f06b9c9
salan223 <salanbhattarai223@gmail.com>
2025-01-11 13:33:12 -0500

    Resolved submodule conflict and added react-portfolio as a normal directory

---
+++ react-portfolio/src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./ProjectCard.module.css";
+import { getImageUrl } from "../../utils";
+
+export const ProjectCard = ({ project }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <img
+          src={getImageUrl(project.imageSrc)}
+          alt={project.title}
+          className={styles.image}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h3 className={styles.title}>{project.title}</h3>
+        <p className={styles.description}>{project.description}</p>
+        <ul className={styles.skills}>
+          {project.skills.map((skill, index) => (
+            <li key={index} className={styles.skill}>{skill}</li>
+          ))}
+        </ul>
+        <div className={styles.links}>
+          <a href={project.demo} className={styles.link}>Demo</a>
+          <a href={project.source} className={styles.link}>Source</a>
+        </div>
+      </div>
+    </div>
+  );
+};
